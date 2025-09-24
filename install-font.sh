dc50e70705d2193042e4732789e1d5a36716d8e7
tilen.okretic <tilen.okretic@gmail.com>
2025-09-24 21:57:59 +0200

    update(install-font): added some functionality comments

--- install-font.sh
+++ install-font.sh
@@ -4,12 +4,15 @@ set -xe
 
 FONT_FILE_NAME=PkgTTF-IosevkaTermSS02-33.2.1.zip
 
+# Create font home location
 mkdir $HOME/.local/share/fonts/iosevka-SS02-term
 
 cd $HOME/.local/share/fonts/iosevka-SS02-term
 
+# Download the font
 wget https://github.com/be5invis/Iosevka/releases/download/v33.2.1/$FONT_FILE_NAME 
 
+# Expand the font files
 unzip $FONT_FILE_NAME
 
 echo "Done!"
